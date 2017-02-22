d77a3f75a13b95c36f765d0ea2e1b9427503657f
Mrbysco <Heeybysco@live.nl>
2017-02-22 16:36:27 +0100

    Versioning
    new version \o/

--- src/main/java/matgm50/mankini/lib/ModLib.java
+++ src/main/java/matgm50/mankini/lib/ModLib.java
@@ -8,7 +8,7 @@ public class ModLib {
 
     public static final String MOD_ID = "mankini";
     public static final String MOD_NAME = "Mankini";
-    public static final String VERSION = "1.3.1";
+    public static final String VERSION = "1.3.2";
 	public static final String ACCEPTED_VERSIONS = "[1.10.2]";
 
     public static final String CLIENTPROXY = "matgm50.mankini.proxy.ClientProxy";
