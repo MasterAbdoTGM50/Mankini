299960bc7e7b8bfa2ef4de3ca184e0657a42cc82
Mrbysco <Heeybysco@live.nl>
2017-02-22 23:46:58 +0100

    No rubberbanding allowed :)

--- src/main/java/matgm50/mankini/init/ModEntities.java
+++ src/main/java/matgm50/mankini/init/ModEntities.java
@@ -11,11 +11,11 @@ import net.minecraftforge.fml.common.registry.EntityRegistry;
 public class ModEntities {
 	public static void register() {
 
-        EntityRegistry.registerModEntity(EntityMankiniCapsule.class, "MankiniCapsule", 0, Mankini.instance, 64, 10, true);
-        EntityRegistry.registerModEntity(EntityMankiniCreeper.class, "MankiniCreeper", 1, Mankini.instance, 64, 10, true);
-        EntityRegistry.registerModEntity(EntityMankiniSpider.class, "MankiniSpider", 2, Mankini.instance, 64, 10, true);
-        EntityRegistry.registerModEntity(EntityMankiniEnderman.class, "MankiniEnderman", 3, Mankini.instance, 64, 10, true);
-        EntityRegistry.registerModEntity(EntityMankiniEndermite.class, "MankiniEnderMite", 4, Mankini.instance, 64, 10, true);
+        EntityRegistry.registerModEntity(EntityMankiniCapsule.class, "MankiniCapsule", 0, Mankini.instance, 64, 3, true);
+        EntityRegistry.registerModEntity(EntityMankiniCreeper.class, "MankiniCreeper", 1, Mankini.instance, 80, 3, true);
+        EntityRegistry.registerModEntity(EntityMankiniSpider.class, "MankiniSpider", 2, Mankini.instance, 80, 3, true);
+        EntityRegistry.registerModEntity(EntityMankiniEnderman.class, "MankiniEnderman", 3, Mankini.instance, 80, 3, true);
+        EntityRegistry.registerModEntity(EntityMankiniEndermite.class, "MankiniEnderMite", 4, Mankini.instance, 80, 3, true);
         //registerEntity(EntityMankiniWither.class, "Mankini Wither");
         
     }
