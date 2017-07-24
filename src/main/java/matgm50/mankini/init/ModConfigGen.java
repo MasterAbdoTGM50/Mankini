15c7c1b874e6fbcff8338b585bc1265ab12f73e6
Mrbysco <Heeybysco@live.nl>
2017-07-24 23:45:06 +0200

    Swap config

--- src/main/java/matgm50/mankini/init/ModConfigGen.java
+++ src/main/java/matgm50/mankini/init/ModConfigGen.java
@@ -1,13 +1,21 @@
 package matgm50.mankini.init;
 
-import net.minecraftforge.common.config.Configuration;
+import matgm50.mankini.lib.ModLib;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+@Config(modid = ModLib.MOD_ID)
 public class ModConfigGen {
-	public static void configOptions(Configuration config) {
-		config.load();
-		// Config options here
-		
-		config.save();
-		config.load();
+
+	private static class EventHandler {
+
+		@SubscribeEvent
+		public static void onConfigChanged(final ConfigChangedEvent.OnConfigChangedEvent event) {
+			if (event.getModID().equals(ModLib.MOD_ID)) {
+				ConfigManager.load(ModLib.MOD_ID, Config.Type.INSTANCE);
+			}
+		}
 	}
 }
