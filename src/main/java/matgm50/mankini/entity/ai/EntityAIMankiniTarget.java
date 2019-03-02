e149cf37488f61d0888c0807477a3ead01e59f09
Mrbysco <Heeybysco@live.nl>
2019-03-03 00:35:28 +0100

    Fix the inverted output

--- src/main/java/matgm50/mankini/entity/ai/EntityAIMankiniTarget.java
+++ src/main/java/matgm50/mankini/entity/ai/EntityAIMankiniTarget.java
@@ -1,43 +1,29 @@
 package matgm50.mankini.entity.ai;
 
+import matgm50.mankini.Mankini;
 import matgm50.mankini.item.IMankini;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
-import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
 
-import java.util.function.Predicate;
-
 public class EntityAIMankiniTarget<T extends EntityLivingBase> extends EntityAINearestAttackableTarget<T> {
-    private final EntityCreature attacker;
 
     public EntityAIMankiniTarget(EntityCreature attacker, Class<T> target, boolean checkSight) {
         super(attacker, target, checkSight);
-        this.attacker = attacker;
     }
 
     /**
      * Returns whether the EntityAIBase should begin execution.
      */
     public boolean shouldExecute() {
-        if(isFabulous())
-            return false;
-        else
-            return super.shouldExecute();
+        return super.shouldExecute() && !isFabulous();
     }
 
     public boolean isFabulous() {
-        if(this.target instanceof EntityPlayer) {
-            EntityPlayer player = (EntityPlayer)this.target;
-            boolean flag = player.inventory.armorInventory.get(2).getItem() instanceof IMankini;
-
-            if(!flag) {
-                return true;
-            }
-            else {
-                return false;
-            }
+        if(this.targetEntity != null && this.targetEntity instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer)this.targetEntity;
+            return player.inventory.armorInventory.get(2).getItem() instanceof IMankini;
         }
         else {
             return false;
