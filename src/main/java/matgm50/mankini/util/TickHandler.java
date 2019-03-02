65b21e89107e42a0882cc962a5b0fc309d0282e2
Mrbysco <Heeybysco@live.nl>
2019-03-02 20:31:14 +0100

    Anti Wither

--- src/main/java/matgm50/mankini/util/TickHandler.java
+++ src/main/java/matgm50/mankini/util/TickHandler.java
@@ -1,10 +1,13 @@
 package matgm50.mankini.util;
 
+import matgm50.mankini.init.ModEffects;
 import matgm50.mankini.init.ModItems;
 import matgm50.mankini.lib.ModLib;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -24,10 +27,16 @@ public class TickHandler {
         ItemStack armor = player.getItemStackFromSlot(EntityEquipmentSlot.CHEST);
 
         if(armor != null) {
-        	if (armor.getItem().equals(ModItems.bat_mankini)) {
+            if (armor.getItem().equals(ModItems.wither_mankini)) {
+                if(player.getActivePotionEffects().contains(MobEffects.WITHER) || player.getActivePotionEffects().contains(ModEffects.MANKINI_WITHER)) {
+                    player.removePotionEffect(MobEffects.WITHER);
+                    player.removePotionEffect(ModEffects.MANKINI_WITHER);
+                }
+            }
+            if (armor.getItem().equals(ModItems.bat_mankini)) {
                 player.fallDistance = 0F;
             }
-        	
+
             if(armor.getItem().equals(ModItems.aetheric_mankini)) {
             	allowFlying = true;
             }
