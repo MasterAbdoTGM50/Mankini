99745a0438738bc844c88595a220a0b3236b971a
Mrbysco <Mrbysco@users.noreply.github.com>
2023-04-04 16:16:09 +0200

    Update datagen

--- src/main/java/matgm50/mankini/data/MankiniDatagen.java
+++ src/main/java/matgm50/mankini/data/MankiniDatagen.java
@@ -17,6 +17,7 @@ import net.minecraft.data.loot.EntityLootSubProvider;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.data.registries.VanillaRegistries;
 import net.minecraft.resources.RegistryOps;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.flag.FeatureFlags;
@@ -29,8 +30,8 @@ import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.common.data.JsonCodecProvider;
 import net.minecraftforge.common.world.BiomeModifier;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -38,10 +39,10 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Stream;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -49,22 +50,12 @@ public class MankiniDatagen {
 	@SubscribeEvent
 	public static void gatherData(GatherDataEvent event) {
 		HolderLookup.Provider provider = getProvider();
-		final RegistryOps<JsonElement> ops = RegistryOps.create(JsonOps.INSTANCE, provider);
 		DataGenerator generator = event.getGenerator();
 		PackOutput packOutput = generator.getPackOutput();
-		ExistingFileHelper helper = event.getExistingFileHelper();
 
 		if (event.includeServer()) {
-			Map<ResourceLocation, BiomeModifier> biomeModifiers = new LinkedHashMap<>();
-			biomeModifiers.putAll(addModifierForType(EntityType.CREEPER, ModRegistry.MANKINI_CREEPER.get(), 4));
-			biomeModifiers.putAll(addModifierForType(EntityType.ENDERMAN, ModRegistry.MANKINI_ENDERMAN.get(), 4));
-			biomeModifiers.putAll(addModifierForType(EntityType.SPIDER, ModRegistry.MANKINI_SPIDER.get(), 4));
-			biomeModifiers.putAll(addModifierForType(EntityType.SKELETON, ModRegistry.MANKINI_SKELETON.get(), 4));
-			biomeModifiers.putAll(addModifierForType(EntityType.EVOKER, ModRegistry.MANKINI_EVOKER.get(), 4));
-
-			generator.addProvider(event.includeServer(), JsonCodecProvider.forDatapackRegistry(
-					packOutput, helper, ModLib.MOD_ID, ops, ForgeRegistries.Keys.BIOME_MODIFIERS, biomeModifiers
-			));
+			generator.addProvider(event.includeServer(), new DatapackBuiltinEntriesProvider(
+					packOutput, CompletableFuture.supplyAsync(MankiniDatagen::getProvider), Set.of(ModLib.MOD_ID)));
 
 			generator.addProvider(event.includeServer(), new Loots(packOutput));
 		}
@@ -73,15 +64,21 @@ public class MankiniDatagen {
 	private static HolderLookup.Provider getProvider() {
 		final RegistrySetBuilder registryBuilder = new RegistrySetBuilder();
 		// We need the BIOME registry to be present so we can use a biome tag, doesn't matter that it's empty
-		registryBuilder.add(Registries.BIOME, $ -> {
+		registryBuilder.add(Registries.BIOME, context -> {
+		});
+		registryBuilder.add(ForgeRegistries.Keys.BIOME_MODIFIERS, context -> {
+			context.register(createModifierKey("creeper"), new AddRelativeSpawnBiomeModifier(EntityType.CREEPER, ModRegistry.MANKINI_CREEPER.get(), 4));
+			context.register(createModifierKey("enderman"), new AddRelativeSpawnBiomeModifier(EntityType.ENDERMAN, ModRegistry.MANKINI_ENDERMAN.get(), 4));
+			context.register(createModifierKey("spider"), new AddRelativeSpawnBiomeModifier(EntityType.SPIDER, ModRegistry.MANKINI_SPIDER.get(), 4));
+			context.register(createModifierKey("skeleton"), new AddRelativeSpawnBiomeModifier(EntityType.SKELETON, ModRegistry.MANKINI_SKELETON.get(), 4));
+			context.register(createModifierKey("evoker"), new AddRelativeSpawnBiomeModifier(EntityType.EVOKER, ModRegistry.MANKINI_EVOKER.get(), 4));
 		});
 		RegistryAccess.Frozen regAccess = RegistryAccess.fromRegistryOfRegistries(BuiltInRegistries.REGISTRY);
 		return registryBuilder.buildPatch(regAccess, VanillaRegistries.createLookup());
 	}
 
-	private static Map<ResourceLocation, BiomeModifier> addModifierForType(EntityType<?> originalType, EntityType<?> newType, int relativeWeight) {
-		return Map.of(ForgeRegistries.ENTITY_TYPES.getKey(originalType), new AddRelativeSpawnBiomeModifier(
-				originalType, newType, relativeWeight));
+	private static ResourceKey<BiomeModifier> createModifierKey(String name) {
+		return ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(ModLib.MOD_ID, name));
 	}
 
 	private static class Loots extends LootTableProvider {
